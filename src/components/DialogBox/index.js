ffd2ad1c69c90cde4feb4aeedf89d04f46bea6f8
Elonmusk <srujanaj313@gmail.com>
2025-02-06 09:45:35 +0530

    Dialog box Changes

--- src/components/DialogBox/index.js
+++ src/components/DialogBox/index.js
@@ -1,49 +1,44 @@
 import * as React from "react";
-import Button from "@mui/material/Button";
+import {Button, Stack, Typography} from "@mui/material";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 
-export default function AlertDialog({ open, handleConfirmSubmit,handleCancel }) {
-  //   const [open, setOpen] = React.useState(false);
-
-  //   const handleClickOpen = () => {
-  //     setOpen(true);
-  //   };
-
-  //   const handleClose = () => {
-  //     setOpen(false);
-  //   };
-
+export default function AlertDialog({
+  open,
+  handleConfirmSubmit,
+  handleCancel,
+}) {
   return (
-    <React.Fragment>
-      {/* <Button variant="outlined" onClick={handleClickOpen}>
-        Open alert dialog
-      </Button> */}
+    <>
       <Dialog
         open={open}
         onClose={handleCancel}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
+        fullWidth
       >
-        <DialogTitle id="alert-dialog-title">
-          {"Confirmation"}
-        </DialogTitle>
+        <DialogTitle id="alert-dialog-title" sx={{color:"#183084", fontWeight:"bold"}}>{"Confirmation"}</DialogTitle>
         <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-           Do you Want to ReAssign all?
-          </DialogContentText>
+          <Stack
+            justifyContent="flex-start"
+            alignItems="center"
+          >
+            <Typography sx={{fontSize:"15px",color:"#183084", width:"100%"}}>Are you sure, You want to ReAssign all?</Typography>
+          </Stack>
         </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCancel}>Cancel</Button>
-          <Button onClick={handleConfirmSubmit} autoFocus>
+        <DialogActions sx={{ margin: 2 }}>
+          <Button variant="contained" onClick={() => handleConfirmSubmit()}>
             Confirm
+          </Button>
+          <Button variant="outlined" onClick={() => handleCancel()}>
+            Cancel
           </Button>
         </DialogActions>
       </Dialog>
-    </React.Fragment>
+    </>
   );
 }
 
